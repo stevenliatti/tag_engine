08c218b9eb2e3664baeebc74fab85149fdcd93fc
steven.liatti <steven.liatti@etu.hesge.ch>
2018-06-06 22:09:05 +0200

    A day spent on try to get a map, I commit basic visit_dirs and print files and tags On branch master
    Changes to be committed:
    	new file:   .gitignore
    	new file:   Cargo.lock
    	modified:   Cargo.toml
    	modified:   src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,47 @@
+use std::io;
+use std::fs;
+use std::path::Path;
+
+extern crate tag_manager;
+
+fn visit_dirs(dir: &Path, level: u32) -> io::Result<()> {
+    let next_level = level + 1;
+    if dir.is_dir() {
+        for entry in fs::read_dir(dir)? {
+            let entry = entry?;
+            let path = entry.path();
+            for _ in 0..level {
+                print!("    ");
+            }
+            if path.is_dir() {
+                println!("dir {:?}", path);
+                visit_dirs(&path, next_level)?;
+            }
+            else {
+                println!("file {:?}", path);
+            }
+            let option = tag_manager::get_tags(dir.to_str().unwrap());
+            match option {
+                Some(tags) => {
+                    for _ in 0..level {
+                        print!("    ");
+                    }
+                    println!("Tag(s) {:?} for file {:?}", tags, path);
+                },
+                None => ()
+            }
+        }
+    }
+    Ok(())
+}
+
 fn main() {
-    println!("Hello, world!");
+    // mkdir -p a/b/c
+    // touch fileA fileB
+    let path = Path::new("a");
+
+    match visit_dirs(path, 0) {
+        Ok(_) => (),
+        Err(err) => println!("error : {}", err)
+    }
 }
