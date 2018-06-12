5463bd0d2492bf251f25d0ba8add149e766f777a
steven.liatti <steven.liatti@etu.hesge.ch>
2018-06-12 13:13:58 +0200

    Implement basic index path -> tags with HashMap for now. Need to improve this data structure
    On branch master
    
    Changes to be committed:
    	modified:   src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,8 @@ use walkdir::WalkDir;
 fn main() {
     // mkdir -p a/b/c
     // touch fileA fileB
-
-    let mut map : HashMap<String, HashSet<String>> = HashMap::new();
+    let mut tag_to_paths : HashMap<String, HashSet<String>> = HashMap::new();
+    let mut path_to_tags : HashMap<String, HashSet<String>> = HashMap::new();
 
     for entry in WalkDir::new("a").into_iter().filter_map(|e| e.ok()) {
         let path = entry.path().display().to_string();
@@ -17,8 +17,9 @@ fn main() {
         let option = tag_manager::get_tags(&path);
         match option {
             Some(tags) => {
+                path_to_tags.insert(path.clone(), tags.clone());
                 for tag in tags {
-                    match map.entry(tag) {
+                    match tag_to_paths.entry(tag) {
                         Vacant(entry) => {
                             let mut set = HashSet::new();
                             set.insert(path.clone());
@@ -30,9 +31,10 @@ fn main() {
                     }
                 }
             },
-            None => ()
+            None => { path_to_tags.insert(path.clone(), HashSet::new()); }
         } 
     }
 
-    println!("map {:?}", map);
+    println!("tag_to_paths {:#?}", tag_to_paths);
+    println!("path_to_tags {:#?}", path_to_tags);
 }
