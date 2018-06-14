e24e4d76c7a938c6d42a03f688169e25ecf25a3f
steven.liatti <steven.liatti@etu.hesge.ch>
2018-06-14 22:46:31 +0200

    Finally a way to establish a structure between tags, files and directories ! Not fully functionnal, but it's something
    On branch master
    
    Changes to be committed:
    	modified:   Cargo.toml
    	modified:   src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,55 +1,92 @@
-use std::collections::{HashMap, HashSet};
-use std::collections::hash_map::Entry::{Vacant, Occupied};
-use std::fs::FileType;
+use std::collections::HashMap;
+use std::collections::hash_map::Entry::Vacant;
+// use std::fs::FileType;
 
 extern crate tag_manager;
 extern crate walkdir;
 use walkdir::WalkDir;
 
+extern crate petgraph;
+use petgraph::Graph;
+use petgraph::graph::NodeIndex;
+use petgraph::Direction;
+
 #[derive(Debug)]
-struct Entry {
-    filetype : FileType,
-    tags : HashSet<String>
+enum Node {
+    Tag(String),
+    File(String),
+    Directory(String)
 }
 
-impl Entry {
-    fn new(filetype : FileType, tags : HashSet<String>) -> Self {
-        Self { filetype : filetype, tags : tags }
+fn find_parent(graph : &Graph<Node, String>, index : NodeIndex, entry : &str) -> NodeIndex {
+    for neighbor_index in graph.neighbors_directed(index, Direction::Outgoing) {
+        match graph.node_weight(neighbor_index) {
+            Some(data) => {
+                match data {
+                    // TODO: maybe no need of or (directory only ?)
+                    &Node::File(ref name) | &Node::Directory(ref name) => {
+                        println!("name {}", name);
+                        if String::from(entry) == name.to_string() {
+                            return neighbor_index;
+                        }
+                        else {
+                            return index;
+                        }
+                    },
+                    _ => ()
+                }
+            },
+            None => ()
+        }
     }
+    index
 }
 
 fn main() {
-    // mkdir -p a/b/c
-    // touch fileA fileB
-    let mut tag_to_paths : HashMap<String, HashSet<String>> = HashMap::new();
-    let mut path_to_tags : HashMap<String, Entry> = HashMap::new();
+    let path_root = "a";
+    let mut graph : Graph<Node, String> = Graph::new();
+    let root_index = graph.add_node(Node::Directory(String::from(path_root)));
+    let mut current_index : NodeIndex;
 
-    for entry in WalkDir::new("a").into_iter().filter_map(|e| e.ok()) {
+    let mut tags_index = HashMap::new();
+
+    for entry in WalkDir::new(path_root).into_iter().filter_map(|e| e.ok()) {
         let path = entry.path().display().to_string();
-        println!("{}", path);
+        println!("path {}", path);
+        
+        let mut path_vec : Vec<&str> = path.split('/').collect();
+        let new_item = path_vec.pop().unwrap();
+        // remove path_root
+        if !path_vec.is_empty() {
+            path_vec.remove(0);
+            current_index = root_index;
+            for entry in path_vec {
+                current_index = find_parent(&graph, current_index, entry);
+            }
+            // TODO: check type (file or directory)
+            let new_node = graph.add_node(Node::Directory(String::from(new_item)));
+            graph.add_edge(current_index, new_node, String::from("yes"));
+        }
+
+        // TODO: add tag to graph
         let option = tag_manager::get_tags(&path);
         match option {
             Some(tags) => {
-                path_to_tags.insert(path.clone(), Entry::new(entry.file_type(), tags.clone()));
                 for tag in tags {
-                    match tag_to_paths.entry(tag) {
+                    match tags_index.entry(tag.clone()) {
                         Vacant(entry) => {
-                            let mut set = HashSet::new();
-                            set.insert(path.clone());
-                            entry.insert(set);
+                            let new_node_tag = graph.add_node(Node::Tag(tag.clone()));
+                            entry.insert(new_node_tag);
                         },
-                        Occupied(mut entry) => {
-                            entry.get_mut().insert(path.clone());
-                        }
+                        _ => ()
                     }
                 }
+
             },
-            None => { 
-                path_to_tags.insert(path.clone(), Entry::new(entry.file_type(), HashSet::new()));
-            }
-        } 
+            None => ()
+        }
     }
 
-    println!("tag_to_paths {:#?}", tag_to_paths);
-    println!("path_to_tags {:#?}", path_to_tags);
+    println!("tags_index {:#?}", tags_index);
+    println!("graph {:#?}", graph);
 }
