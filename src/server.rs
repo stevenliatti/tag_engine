059c977f34f37e4cd9b86a83105ec686aff076da
steven.liatti <steven.liatti@etu.hesge.ch>
2018-07-03 22:13:50 +0200

    Request for entries with "complex" query done. Support "AND" and "OR" operators only, with natural precedence for "AND" on "OR"
    On branch master
    
    Changes to be committed:
    	modified:   src/server.rs

--- src/server.rs
+++ src/server.rs
@@ -1,4 +1,4 @@
-use std::collections::HashMap;
+use std::collections::{HashMap, HashSet};
 use std::io::prelude::*;
 use std::sync::{Mutex, Arc};
 use std::os::unix::net::{UnixListener, UnixStream};
@@ -12,6 +12,7 @@ use petgraph::Direction;
 extern crate tag_manager;
 
 use graph::{MyGraph, NodeKind};
+use parse::{Arg, Operator};
 use parse::infix_to_postfix;
 
 #[derive(Debug, Clone)]
@@ -57,30 +58,34 @@ fn parse_request(stream : &mut UnixStream) -> Option<RequestKind> {
     None
 }
 
-fn make_path(graph : &MyGraph, entry : NodeIndex, path_vec : &mut Vec<String>) {
+fn make_path_vec(graph : &MyGraph, entry : NodeIndex, path_vec : &mut Vec<String>) {
     path_vec.push(graph.node_weight(entry).unwrap().name.clone());
     for neighbor in graph.neighbors_directed(entry, Direction::Incoming) {
         match graph.node_weight(neighbor).unwrap().kind {
             NodeKind::Directory => {
-                make_path(graph, neighbor, path_vec);
+                make_path_vec(graph, neighbor, path_vec);
             },
             _ => ()
         }
     }
 }
 
+fn make_path(graph : &MyGraph, entry : NodeIndex, base_path : String) -> String {
+    let mut path_vec = Vec::new();
+    make_path_vec(&graph, entry, &mut path_vec);
+    let mut path = base_path.clone();
+    for entry in path_vec.into_iter().rev() {
+        path.push_str(&entry);
+        path.push_str("/");
+    }
+    path.pop();
+    path
+}
+
 fn entries(graph : &MyGraph, tag_index : NodeIndex, base_path : String) -> Vec<String> {
     let mut nodes_names = Vec::new();
     for entry in graph.neighbors(tag_index) {
-        let mut path_vec = Vec::new();
-        make_path(&graph, entry, &mut path_vec);
-        let mut path = base_path.clone();
-        for entry in path_vec.into_iter().rev() {
-            path.push_str(&entry);
-            path.push_str("/");
-        }
-        path.pop();
-        nodes_names.push(path);
+        nodes_names.push(make_path(graph, entry, base_path.clone()));
     }
     nodes_names.sort();
     nodes_names
@@ -98,24 +103,55 @@ fn write_response(entries : Vec<String>, stream : &mut UnixStream) {
     stream.flush().unwrap();
 }
 
+fn expression_to_entries(request : String, graph : &MyGraph, tags_index : &HashMap<String, 
+    NodeIndex>, base_path : String) -> Vec<String> {
+    let postfix = infix_to_postfix(request.clone());
+    let mut stack = Vec::new();
+    for arg in postfix {
+        match arg {
+            Arg::Operand(tag) => {
+                if tags_index.contains_key(&tag) {
+                    let tag_index = tags_index.get(&tag).unwrap();
+                    let tags_set : HashSet<NodeIndex> = graph.neighbors(*tag_index).collect();
+                    stack.push(tags_set);
+                }
+                else { stack.push(HashSet::new()); }
+            },
+            Arg::Operator(op) => {
+                if stack.len() >= 2 {
+                    let operand_two = stack.pop().unwrap();
+                    let operand_one = stack.pop().unwrap();
+                    match op {
+                        Operator::AND => stack.push(operand_one.intersection(&operand_two).map(|e| *e).collect()),
+                        Operator::OR => stack.push(operand_one.union(&operand_two).map(|e| *e).collect())
+                    }
+                }
+            }
+        }
+    }
+    let mut nodes_names = Vec::new();
+    if stack.len() == 1 {
+        for entry in stack.pop().unwrap() {
+            nodes_names.push(make_path(graph, entry, base_path.clone()));
+        }
+        nodes_names.sort();
+    }
+    nodes_names
+}
+
 fn request_entries(request : String, graph_thread : &Arc<Mutex<MyGraph>>, 
     tags_index_thread : &Arc<Mutex<HashMap<String, NodeIndex>>>, base_path : String, 
     stream : &mut UnixStream) {
     println!("Request for Entries {:?}", request);
     let graph = graph_thread.lock().unwrap();
     let tags_index = tags_index_thread.lock().unwrap();
-    // TODO:
-    let _postfix = infix_to_postfix(request.clone());
-    println!("NodeIndex {:?}", tags_index.get(&request));
-    match tags_index.get(&request) {
-        Some(index) => {
-            let entries = entries(&graph, *index, base_path.clone());
-            write_response(entries, stream);
-        },
-        None => {
-            stream.write("No files\n".as_bytes()).unwrap();
-            stream.flush().unwrap();
-        }
+    let entries = expression_to_entries(request, &graph, &tags_index, base_path);
+    if entries.is_empty() {
+        stream.write("No files\n".as_bytes()).unwrap();
+        stream.flush().unwrap();
+    }
+    else {
+        write_response(entries, stream);
     }
 }
 
