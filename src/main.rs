80086b2643a848e3f84e00dbab75991e62ecbd7a
steven.liatti <steven.liatti@etu.hesge.ch>
2018-07-03 15:25:32 +0200

    Try implementation of infix to postfix algo, need to test now. I'm going to move functions in src/lib.rs to test with Rust
    On branch master
    
    Changes to be committed:
    	modified:   src/main.rs

--- src/main.rs
+++ src/main.rs
@@ -415,6 +415,73 @@ fn write_response(entries : Vec<String>, stream : &mut UnixStream) {
     stream.flush().unwrap();
 }
 
+const AND_OPERATOR_STR : &str = "AND";
+const OR_OPERATOR_STR : &str = "OR";
+
+#[derive(Debug, Clone)]
+enum Operator { AND, OR }
+
+use Operator::*;
+impl Operator {
+    fn compare(&self, other : &Operator) -> i8 {
+        match (self, other) {
+            (&AND, &OR) => 1,
+            (&OR, &AND) => -1,
+            _ => 0
+        }
+    }
+
+    fn str_to_operator(op_str : &str) -> Option<Self> {
+        if op_str == AND_OPERATOR_STR {
+            Some(AND)
+        }
+        else if op_str == OR_OPERATOR_STR {
+            Some(OR)
+        }
+        else {
+            None
+        }
+    }
+
+    fn operator_to_str(op : &Operator) -> String {
+        match op {
+            &AND => AND_OPERATOR_STR.to_string(),
+            &OR => OR_OPERATOR_STR.to_string()
+        }
+    }
+}
+
+fn infix_to_postfix(infix : String) -> Vec<String> {
+    let infix : Vec<&str> = infix.split(' ').collect();
+    let mut stack = Vec::new();
+    let mut postfix = Vec::new();
+    for arg in infix {
+        if arg == AND_OPERATOR_STR || arg == OR_OPERATOR_STR {
+            let arg = Operator::str_to_operator(arg).unwrap();
+            if stack.is_empty() {
+                stack.push(arg);
+            }
+            else {
+                while !stack.is_empty() {
+                    let mut top_stack = stack.get(stack.len() - 1).unwrap().clone();
+                    let mut compare = arg.compare(&top_stack);
+                    if compare > 0 {
+                        break;
+                    }
+                    else {
+                        postfix.push(Operator::operator_to_str(&stack.pop().unwrap()));
+                    }
+                }
+                stack.push(arg);
+            }
+        }
+        else {
+            postfix.push(arg.to_string());
+        }
+    }
+    postfix.into_iter().map(|e| e.to_string()).collect()
+}
+
 fn socket_server(base_path : String, graph : &Arc<Mutex<MyGraph>>, tags_index : &Arc<Mutex<HashMap<String, NodeIndex>>>) {
     let listener = UnixListener::bind("/tmp/tag_engine").unwrap();
     let graph_thread = Arc::clone(graph);
@@ -428,6 +495,8 @@ fn socket_server(base_path : String, graph : &Arc<Mutex<MyGraph>>, tags_index : 
                     println!("Request for Entries {:?}", request);
                     let graph = graph_thread.lock().unwrap();
                     let tags_index = tags_index_thread.lock().unwrap();
+                    // TODO:
+                    let postfix = infix_to_postfix(request.clone());
                     println!("NodeIndex {:?}", tags_index.get(&request));
                     match tags_index.get(&request) {
                         Some(index) => {
